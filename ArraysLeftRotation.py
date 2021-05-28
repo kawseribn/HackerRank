4a70e31b70d8c29f1e545af793424b6345d76c92
kawseribn <46264704+kawseribn@users.noreply.github.com>
2021-05-28 20:07:11 +0600

    Add files via upload

---
+++ ArraysLeftRotation.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+def rotLeft(a, d):
+    # Write your code here
+    if len(a)>=1 and len(a)<=10**5:
+      if d>=1 and d<=n:
+        if max(a)<=10**6 and min(a)>=1:
+            x = a[0:d]
+            y = a[d:]
+            a=y
+            a.extend(x)
+    return a
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
